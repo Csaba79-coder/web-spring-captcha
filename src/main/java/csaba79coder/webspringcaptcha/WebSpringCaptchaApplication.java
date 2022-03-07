582e8b6e3f7e80a50fdecdcb5e9568c7108caa6a
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-03-07 11:10:55 +0100

    Initial commit

---
+++ src/main/java/csaba79coder/webspringcaptcha/WebSpringCaptchaApplication.java
@@ -0,0 +1,13 @@
+package csaba79coder.webspringcaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringCaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSpringCaptchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csaba79coder/webspringcaptcha/WebSpringCaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package csaba79coder.webspringcaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringCaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
