723c8c8bf42a767865b553ae7c830915c2a6a0f8
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-03-07 12:52:26 +0100

    create Login page + security config

---
+++ src/main/java/csaba79coder/webspringcaptcha/controller/LoginController.java
@@ -0,0 +1,13 @@
+package csaba79coder.webspringcaptcha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
